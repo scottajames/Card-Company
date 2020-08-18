d0ab6bd294954dfecad32cd478af8c1eba48813c
Scott James <scottaustinjames@gmail.com>
2020-08-18 15:36:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+<title>Greeting Cards</title>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+<link rel=stylesheet type="text/css" href="style.css">
+</head>
+
+<body>
+
+<header>
+
+	<div class="background">
+		<img src="background.png">
+	</div>
+
+	<div class="navigation-bar">
+		<img src="Sass.png">
+			<a href="#">Home</a>
+				<a href="#">Services</a>
+					<a href="#">Gallery</a>
+						<a href="#">Contact</a>
+	</div>
+
+	<div class="about-text">
+		<h2>Hello I'm Sandra</h2>
+			<h1>I hand craft cards for all occasions</h1>
+				<h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</h3>
+					<a href="#">Contact</a>
+	</div>
+</header>
+
+	<div id="content">
+
+		<div id="top">
+
+			<div class="top-text">
+				<h2>My Crafts</h2>
+					<div></div>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+			</div>
+					<img src="empty.png">
+		</div>
+
+		<div id="middle">
+
+			<div class="circle-one"></div>
+				<div class="circle-two"></div>
+					<div class="circle-three"></div>
+						<div class="circle-four"></div>
+
+
+			<div class="designing">
+			<h2>Designing</h2>
+
+					<h3>Design</h3><img src="empty.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+
+						<h3>Design</h3><img src="empty.png">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+
+							<h3>Design</h3><img src="empty.png">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+				</div>
+
+			<div class="middle-card-image">
+				<img src="empty.png">
+			</div>
+
+			<div class="services">
+				<h2>Services</h2>
+				<div></div>
+
+					<h3>Service</h3><img src="empty.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+
+						<h3>Service</h3><img src="empty.png">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+
+							<h3>Service</h3><img src="empty.png">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+
+			</div>
+
+		</div>
+
+		<div id="bottom">
+
+			<div class="bottom-text">
+				<h2>My Awards</h2>
+					<div></div>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea </p>
+							<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</h3>
+								<a href="#">Gallery</a>
+			</div>
+				<img src="empty.png">
+		</div>
+
+	</div>
+
+<footer>
+
+	<div id="contact">
+	</div>
+
+</footer>
+
+</body>
+
+</html>
